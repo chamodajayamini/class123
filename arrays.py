5d86a3a01e3f1fc6cacd653480afc4083f504b72
chamoda_97 <chamodajayamini19971017@gmail.com>
2023-07-27 20:06:09 +0530

    tenth_commit

---
+++ arrays.py
@@ -0,0 +1,22 @@
+#Python Collections (SIMILAR TO ARRAY)
+
+# LIST, TUPLE, SET, DICTIONARY
+
+#python Lists
+#Lists are used to store the multiple data in a single variable
+#use square brackets
+#Python lists are ordereed
+
+studet_name= ["Chamoda", "Ishini","Rishi","sadun"]
+print(studet_name)
+print(type(studet_name))
+# for i in studet_name:
+#     print(i)
+#Call indexes seperately 
+print(studet_name[0])
+print(studet_name[1])
+print(studet_name[2])
+print(studet_name[3])
+
+    
+
