aac3537935b56e16069256f80ffa75d9d0698c2e
chamoda_97 <chamodajayamini19971017@gmail.com>
2023-07-27 19:16:59 +0530

    fifth_commit

--- numericdata_type.py
+++ numericdata_type.py
@@ -1,7 +1,13 @@
-
+#nUmerical Data Type
 var_1 = 20
 var_2 = 100.75
 var_3 = 100+5j
 print(type(var_1))
 print(type(var_2))
 print(type(var_3))
+
+#scientific Notation
+x = 25e4 # 25*10^4
+print(type(x))
+print(x)
+
